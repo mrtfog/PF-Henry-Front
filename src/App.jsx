8487e8523dffd1545db1d9b62e822f3ba03f2d8f
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2022-09-28 11:07:07 -0300

    creacion de las primeras rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-// import MovieDetail from './components/MovieDetail';
+import MovieDetail from './components/MovieDetail';
 import './_app.scss';
 import Home from './components/Home';
+import { Route, Switch } from 'react-router-dom'
 
 
 function App() {
+
   return (
     <div className="App">
-        <Home />
-        {/* <MovieDetail /> */}
+
+      <Switch>
+
+        <Route exact path='/' component={Home} />
+        <Route exact path='/movies/:id' component={MovieDetail} />
+        
+      </Switch>
+
     </div>
   );
 }
