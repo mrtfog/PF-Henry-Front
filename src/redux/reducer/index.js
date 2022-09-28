576e93312cf851db28a677f37257ccbfe507c431
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2022-09-28 14:14:59 -0300

    correcion de brackets en actions y reducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -6,13 +6,11 @@ const initialState = {
   filterBy: "All",
 }
 
-
-}
-
 export default function rootReducer(state= initialState, action) {
     
 
     switch(action.type){
+
         case 'GET_MOVIES': 
             return{
               ...state,
@@ -20,24 +18,27 @@ export default function rootReducer(state= initialState, action) {
 
             }
 
-    case 'GET_MOVIE_DETAIL':
-           return {
-        ...state,
-        movieOnDisplay: action.payload
-      }
+        case 'GET_MOVIE_DETAIL':
+            return {
+                ...state,
+                movieOnDisplay: action.payload
+            }
 
 
-          case 'ORDER_ASC_NAME':
-          case "ORDER_ASC_RATING":
+        case 'ORDER_ASC_NAME':
+        case "ORDER_ASC_RATING":
+
             return{
               ...state,
               filteredMovies: action.payload.moviesOrder,
               orderBy: action.payload.name,
             }
+
+
         default: 
         return {
             ...state
         }
-  }
+    }
 
 }
