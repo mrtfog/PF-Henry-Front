38e6983ff37a2e652f928f9d8f66a3feaf4148c8
Angela Testino <angela11021997@gmail.com>
2022-09-29 13:48:58 -0500

    flujo  getAllfunctions

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,58 +1,53 @@
-import axios from 'axios'
+import axios from "axios";
 
-export function getMovies(){
+export function getMovies() {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get("http://localhost:8082/api/popular");
 
-    return async(dispatch)=>{
-
-        try{
-
-            const {data} = await axios.get('http://localhost:8082/api/popular')
-
-            return dispatch({type: 'GET_MOVIES', payload: data })
-
-        }
-
-        catch(e){
-
-            console.log(e)
-        }
-
+      return dispatch({ type: "GET_MOVIES", payload: data });
+    } catch (e) {
+      console.log(e);
     }
-    
-} 
-
-export function getMovieDetail(id){
-
-    return async(dispatch)=>{
-
-        try{
-
-            const {data} = await axios.get(`http://localhost:8082/api/${id}`)
-
-            return dispatch({type: 'GET_MOVIE_DETAIL', payload: data})
-
-        }
-
-        catch(e){
-
-            console.log(e)
-        }
-
-    }
+  };
 }
 
+export function getMovieDetail(id) {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`http://localhost:8082/api/${id}`);
 
-export function getMoviesByName(payload){
-    return {
-        type: 'GET_MOVIE_BY_NAME',
-        payload
+      return dispatch({ type: "GET_MOVIE_DETAIL", payload: data });
+    } catch (e) {
+      console.log(e);
     }
+  };
 }
 
+export function getMoviesByName(payload) {
+  return {
+    type: "GET_MOVIE_BY_NAME",
+    payload,
+  };
+}
 
-export function sortByName(payload){
-    return {
-        type: 'FILTER_BY_NAME',
-        payload
+export function sortByName(payload) {
+  return {
+    type: "FILTER_BY_NAME",
+    payload,
+  };
+}
+
+//FUNCTION ACTIONS
+
+export function getAllFunctions() {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get("http://localhost:8082/function/getAll");
+
+      return dispatch({ type: "GET_FUNCTIONS", payload: data });
+    } catch (e) {
+      console.log(e);
     }
+  };
 }
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,61 +1,71 @@
 const initialState = {
-  movies:[],
+  movies: [],
   allMovies: [],
-  movieOnDisplay:  {},
-}
+  movieOnDisplay: {},
+  functions: [],
+};
 
-export default function rootReducer(state= initialState, action) {
-    
-
-    switch(action.type){
-
-        case 'GET_MOVIES': 
-        let filteredMovies = action.payload.filter( m => {
-            if(m.language !== "ko"){return m}
-        })
-            return{
-              ...state,
-              movies: filteredMovies, 
-              allMovies: filteredMovies  
-            }
-
-        case 'GET_MOVIE_DETAIL':
-            return {
-                ...state,
-                movieOnDisplay: action.payload
-            }
-     
-        case 'GET_MOVIE_BY_NAME':
-          let searchResult = action.payload.length ? state.allMovies.filter(movie => {
-          return movie.title.toUpperCase().includes(action.payload.toUpperCase())===true})
-          : state.allMovies
-          return{
-          ...state,
-          movies: searchResult
-          }
-      
-
-          case 'FILTER_BY_NAME':
-            let sortedByName = action.payload === 'ASC' ?
-                state.movies.sort(function (a,b){
-                    if (a.title > b.title) return -1;
-                    if (a.title < b.title) return 1;
-                    else return 0
-                }) :
-                state.movies.sort(function (a,b){
-                    if (a.title > b.title) return 1;
-                    if (a.title < b.title) return -1;
-                    else return 0
-                })
-            return{
-                ...state,
-                movies: sortedByName
-            }
-
-        default: 
-        return {
-            ...state
+export default function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case "GET_MOVIES":
+      let filteredMovies = action.payload.filter((m) => {
+        if (m.language !== "ko") {
+          return m;
         }
-    }
+      });
+      return {
+        ...state,
+        movies: filteredMovies,
+        allMovies: filteredMovies,
+      };
 
+    case "GET_MOVIE_DETAIL":
+      return {
+        ...state,
+        movieOnDisplay: action.payload,
+      };
+
+    case "GET_MOVIE_BY_NAME":
+      let searchResult = action.payload.length
+        ? state.allMovies.filter((movie) => {
+            return (
+              movie.title
+                .toUpperCase()
+                .includes(action.payload.toUpperCase()) === true
+            );
+          })
+        : state.allMovies;
+      return {
+        ...state,
+        movies: searchResult,
+      };
+
+    case "FILTER_BY_NAME":
+      let sortedByName =
+        action.payload === "ASC"
+          ? state.movies.sort(function (a, b) {
+              if (a.title > b.title) return -1;
+              if (a.title < b.title) return 1;
+              else return 0;
+            })
+          : state.movies.sort(function (a, b) {
+              if (a.title > b.title) return 1;
+              if (a.title < b.title) return -1;
+              else return 0;
+            });
+      return {
+        ...state,
+        movies: sortedByName,
+      };
+    case "GET_FUNCTIONS":
+      return {
+        ...state,
+        functions: action.payload,
+      };
+
+    default:
+      return {
+        ...state,
+      };
+  }
 }
