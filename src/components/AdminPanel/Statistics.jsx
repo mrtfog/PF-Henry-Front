1e1d5884b51a928d820f45ac9c8467f2a0ad9ea0
Angela Testino <angela11021997@gmail.com>
2022-10-05 18:06:28 -0500

    fix on navadmin

--- src/components/AdminPanel/Statistics.jsx
+++ src/components/AdminPanel/Statistics.jsx
@@ -5,7 +5,9 @@ import style from "../../scss/components/AdminPanel/_statistics.module.scss";
 const Statistics = () => {
   return (
     <div className={style.container_statistics}>
-      <h2>Statistics</h2>
+      <div>
+        <h2>Statistics</h2>
+      </div>
     </div>
   );
 };
---
+++ src/components/AdminPanel/Users.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "../../scss/components/AdminPanel/_users.module.scss";
+
+const Users = () => {
+  return (
+    <div className={style.container_users}>
+      <div>
+        <h2>Users</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Users;
