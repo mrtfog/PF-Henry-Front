8db4badd85d7daebc458e8fc0678239c82ce813a
Angela Testino <angela11021997@gmail.com>
2022-10-05 12:53:15 -0500

    navbar admin

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,36 @@ import { Route, Switch } from "react-router-dom";
 import MovieDetail from "./components/MovieDetail";
 import Navbar from "./components/Navbar";
 import Function from "./components/Forms/Function";
+import Statistics from "./components/AdminPanel/Statistics";
+import NavbarAdmin from "./components/AdminPanel/Navbar";
+import { useLocation } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/movies/:id" component={MovieDetail} />
-        <Route exact path="/function" component={Function} />
-      </Switch>
-    </div>
-  );
+  const { pathname } = useLocation();
+  if (!pathname.includes("/admin")) {
+    return (
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies/:id" component={MovieDetail} />
+        </Switch>
+      </div>
+    );
+  }
+  if (pathname.includes("/admin")) {
+    return (
+      <div className="AppAdmin">
+        <NavbarAdmin />
+        <Switch>
+          {/* RUTAS ADMIN */}
+
+          <Route exact path="/admin/statistics" component={Statistics} />
+          <Route exact path="/admin/function" component={Function} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
