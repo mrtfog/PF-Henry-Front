149868e9c1280f21e9522d15b3fa525b8ac83ffe
Martin Fogliacco <fogliaccomartin3@gmail.com>
2022-10-06 16:26:11 -0300

    routing to cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,13 +10,14 @@ export default function Cart() {
     const functions = useSelector((state) => state.showtimesReducer.showtimes);
     // console.log(functions)
 
-    use
-
-  return (
-    <div>
-
-        ESTE ES EL CART
-        
-    </div>
-  )
+    return (
+        <div>
+        <div>Cart</div>
+        <form action = "http://localhost:8082/payment/payment" method = "POST">
+            <input type = "hidden" name = "title" value = "Minnios" ></input>
+            <input type = "hidden" name =  "price" value = "780"></input>
+            <button type = "submit" style = {{width :"80", height:"40px", borderRadius: "5px"}}>Buy Now</button>
+        </form>
+        </div>
+      )
 }
