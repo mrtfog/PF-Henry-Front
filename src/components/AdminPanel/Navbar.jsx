dc19601ead81dee90d8b2e26ac17d35e96313237
Angela Testino <angela11021997@gmail.com>
2022-10-10 18:58:42 -0500

    navbar admin done

--- src/components/AdminPanel/Navbar.jsx
+++ src/components/AdminPanel/Navbar.jsx
@@ -89,17 +89,37 @@ const NavbarAdmin = () => {
               onMouseLeave={() => setIsShown(false)}
             >
               <svg
-                className={style.dropDownIcon}
-                id="dropDownIcon"
+                className={!isShown ? style.dropDownIcon : style.dropFaceDown}
                 xmlns="http://www.w3.org/2000/svg"
-                width="24px"
-                height="24px"
-                viewBox="0 0 24 24"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                version="1.1"
+                id="Capa_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 492.432 492.432"
+                xmlSpace="preserve"
               >
-                <rect x="0" fill="none" width="24" height="24" />
-                <g>
-                  <path d="M7 10l5 5 5-5" />
+                <g id="XMLID_134_">
+                  <path
+                    id="XMLID_135_"
+                    d="M142.238,492.432c-9.79,0-19.588-3.736-27.05-11.209c-14.945-14.934-14.945-39.162,0-54.098l180.9-180.909   l-180.9-180.91c-14.945-14.935-14.945-39.163,0-54.098c14.926-14.944,39.172-14.944,54.098,0l207.96,207.958   c14.943,14.935,14.943,39.164,0,54.1l-207.96,207.957C161.824,488.697,152.026,492.432,142.238,492.432z"
+                  />
                 </g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
+                <g></g>
               </svg>
             </div>
           </NavLink>
