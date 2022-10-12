6cfb2ad6f1673b49d5ccc0edb3adadb47e7137e4
Angela Testino <angela11021997@gmail.com>
2022-10-12 18:48:50 -0500

    unimportant commit just ignore

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import style from "../../scss/components/Cart/_cart.module.scss";
-import { getCart, selectSeatsDisplay, selectedReservation, clearCart, clearCartByMovie } from "../../redux/actions/cart";
+import {
+  getCart,
+  selectSeatsDisplay,
+  selectedReservation,
+  clearCart,
+  clearCartByMovie,
+} from "../../redux/actions/cart";
 import { useAuth } from "../contexts/AuthContext";
 import { useHistory } from "react-router-dom";
 
 export default function Cart() {
-
-  const { currentUser } = useAuth()
-  const history = useHistory()
+  const { currentUser } = useAuth();
+  const history = useHistory();
 
   const dispatch = useDispatch();
 
@@ -16,63 +21,54 @@ export default function Cart() {
 
   useEffect(() => {
     dispatch(getCart());
-    validateConfirm(cart)
+    validateConfirm(cart);
   }, []);
 
-
   function handleOnClick(r) {
-
     if (!currentUser) {
-      alert('To select your seats you need be logged in')
-      history.push('/login')
+      alert("To select your seats you need be logged in");
+      history.push("/login");
     }
-    dispatch(selectSeatsDisplay('flex'))
-    dispatch(selectedReservation(r))
+    dispatch(selectSeatsDisplay("flex"));
+    dispatch(selectedReservation(r));
   }
 
   function handleOnClickReset() {
-    dispatch(clearCart())
+    dispatch(clearCart());
   }
 
   function handleOnClickDeleteMovie(showtimeId) {
-    dispatch(clearCartByMovie(showtimeId))
+    dispatch(clearCartByMovie(showtimeId));
   }
 
   const validateConfirm = (cart) => {
-    const bool = cart.length > 0 ? true : false
-    let seatsBool = true
+    const bool = cart.length > 0 ? true : false;
+    let seatsBool = true;
 
     if (cart.length) {
-
       for (const r of cart) {
-
         if (r.seatsId) {
-          if (r.tickets !== r.seatsId.length) seatsBool = false
+          if (r.tickets !== r.seatsId.length) seatsBool = false;
         } else {
-          seatsBool = false
+          seatsBool = false;
         }
-
       }
-
     }
-    return bool && seatsBool
-
-  }
-
-
+    return bool && seatsBool;
+  };
 
   return (
     <div className={style.container_cart}>
       <div className={style.title}>
         <h2>My Cart</h2>
-        <button className={style.clearCart} onClick={handleOnClickReset}>Clear cart</button>
+        <button className={style.clearCart} onClick={handleOnClickReset}>
+          Clear cart
+        </button>
       </div>
 
       <div className={style.cart}>
         {cart.length ? (
           cart.map((r) => {
-
-
             return (
               <div className={style.movies}>
                 <img src={"https://image.tmdb.org/t/p/original" + r.image} />
@@ -89,14 +85,25 @@ export default function Cart() {
                   Hs
                 </p>
                 <div className={style.seatPicker}>
-                  {
-                    r.seatsId ? <span>Seats selected: <br /> {r.seatsId.join('-')}</span> : <button onClick={() => handleOnClick(r)}>Select your seats</button>
-                  }
+                  {r.seatsId ? (
+                    <span>
+                      Seats selected: <br /> {r.seatsId.join("-")}
+                    </span>
+                  ) : (
+                    <button onClick={() => handleOnClick(r)}>
+                      Select your seats
+                    </button>
+                  )}
                 </div>
                 <p>${Number(r.tickets) * 9.99}</p>
 
                 <div>
-                  <button className={style.delete} onClick={() => handleOnClickDeleteMovie(r.showtimeId)}>X</button>
+                  <button
+                    className={style.delete}
+                    onClick={() => handleOnClickDeleteMovie(r.showtimeId)}
+                  >
+                    X
+                  </button>
                 </div>
               </div>
             );
@@ -110,14 +117,26 @@ export default function Cart() {
         <div className={style.addCombo}>
           {/* <h3 style={{ color: "white" }}>Add combo</h3> */}
         </div>
-        <div className={style.paymentGateway} >
-          <form action="http://pf-henry-back.herokuapp.com/payment/payment" method="POST">
+        <div className={style.paymentGateway}>
+          <form
+            action="http://pf-henry-back.herokuapp.com/payment/payment"
+            method="POST"
+          >
             <input type="hidden" name="title" value="Mininos"></input>
             <input type="hidden" name="price" value="780"></input>
-            <h2>Total: ${cart.reduce((acc, cur) => {
-              return acc = acc + cur.tickets * 9.99
-            }, 0).toFixed(2)}</h2>
-            <button type="submit" className={style.btn_finish} disabled={!validateConfirm(cart)}>
+            <h2>
+              Total: $
+              {cart
+                .reduce((acc, cur) => {
+                  return (acc = acc + cur.tickets * 9.99);
+                }, 0)
+                .toFixed(2)}
+            </h2>
+            <button
+              type="submit"
+              className={style.btn_finish}
+              disabled={!validateConfirm(cart)}
+            >
               Buy Now
             </button>
           </form>
