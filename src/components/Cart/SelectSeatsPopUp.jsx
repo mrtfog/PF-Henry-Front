5ff0cbc7572a1e76b55bfdf4235543c37e0a6945
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2022-10-13 12:40:42 -0300

    (fix) seat picker recibe datos dinamicos

--- src/components/Cart/SelectSeatsPopUp.jsx
+++ src/components/Cart/SelectSeatsPopUp.jsx
@@ -5,19 +5,20 @@ import { getAllRooms } from '../../redux/actions/rooms'
 import { useDispatch, useSelector } from 'react-redux'
 import PopUpTemplate from '../PopUpTemplate';
 import SeatPicker from '../SeatPicker'
+import { getAllShowtimes } from '../../redux/actions/showtimes';
 
 export default function SelectSeatsPopUp() {
     const dispatch = useDispatch()
 
     const display = useSelector( state => state.cartReducer.displaySeats)
     const reservation = useSelector( state => state.cartReducer.selectedReservation)
-
     const movieTheaters = useSelector ( state => state.roomReducer.rooms)
+    const showtime = useSelector(state=> state.showtimesReducer.showtimes).filter(s=>s._id===reservation.showtimeId)
 
-
-
+    console.log('Showtime en popup', showtime)
     useEffect(() => {
       dispatch(getAllRooms())
+      dispatch(getAllShowtimes())
     }, [])
     
 
@@ -33,7 +34,7 @@ export default function SelectSeatsPopUp() {
 
             <div className={style.container_selectSeats}>
                 
-                <SeatPicker reservation={reservation} movieTheaters={movieTheaters}/>
+                <SeatPicker reservation={reservation} movieTheaters={movieTheaters} showtime={showtime}/>
 
             </div>
         )
